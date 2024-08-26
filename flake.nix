528f808888d759c31ad0350b60151e8cbc0ea3d6
nong10 <nong10w@outlook.com>
2024-08-26 06:45:26 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {};
+  in
+  with pkgs.lib;
+  {
+    nixosModules.default = {config, ... }: {
+      options.selinux-refpolicy.configuration = {
+        config.genFile = mkEnableOption "generate /etc/selinux/config";
+        semanage_config.genFile = mkEnableOption "generate /etc/selinux/semanage.conf";
+
+        config.text = mkOption {
+          type = types.str;
+          default = ''
+            SELINUX=disabled
+            SELINUXTYPE=refpolicy
+          '';
+          description = "text inside /etc/selinux/config";
+        };
+        semanage_config.text = mkOption {
+          type = types.str;
+          default = "";
+          description = "text inside /etc/selinux/semanage.conf";
+        };
+      };
+
+      config =  {
+        mkIf config.selinux-refpolicy.configuration.config.genFile {
+          environment.etc."selinux/config".text = ''
+            ${config.selinux-refpolicy.configuration.config.text}
+          '';
+        }
+        mkIf config.selinux-refpolicy.configuration.semanage_config.genFile {
+          environment.etc."selinux/semanage.conf".text = ''
+            ${config.selinux-refpolicy.configuration.semanage_config.text}
+          '';
+        }
+      };
+    };
+
+  };
+}
